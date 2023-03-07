491d3860ab19dadc75732e0015b2457eace5b993
Dave <dskura@gmail.com>
2023-03-07 11:07:52 -0500

    updates

--- tests/zetl_scripts/LoadPostalCodes/2.load_postal_codes.py
+++ tests/zetl_scripts/LoadPostalCodes/2.load_postal_codes.py
@@ -2,21 +2,19 @@
   Dave Skura, 2023
   
 """
-from postgresdave_package.postgresdave import db #install pip install postgresdave-package
+from postgresdave_package.postgresdave import postgres_db #install pip install postgresdave-package
 
-import psycopg2 
 import os
 
 try:
 	tblname = 'weather.postal_codes'
-	mydb = db()
+	mydb = postgres_db()
 	mydb.connect()
 	print (" Connected " ) # 
 	print(mydb.dbversion())
 
 	csvfile = '.\\zetl_scripts\\LoadPostalCodes\\CanadianPostalCodes.csv'
 	print(os.getcwd())
-	#mydb.load_csv_to_table(csvfile,tblname,True,',')
 	print(csvfile)
 
 except Exception as e:
